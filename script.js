c9d0c59df20a0003a7fe6009a0c5fe6bd9b0f76e
francisco marinelli <franciscomarinelli123@hotmail.com>
2023-01-16 11:57:45 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Formulario
+let nombre = document.querySelector("#nombre");
+let email = document.querySelector("#email");
+let formulario = document.querySelector("#formulario");
+let infoForm = document.querySelector(".infoForm");
+
+nombre.addEventListener("input", function(){
+    if(nombre.value === ""){
+        alert("ingrese un nombre")
+    }
+});
+
+email.addEventListener("input", function(){
+    if(email.value === ""){
+        alert("ingrese su correo electronico")
+    }
+});
+
+const mostrarInfoForm = formulario.addEventListener("submit", function (evento){
+    evento.preventDefault();
+    infoForm.innerHTML = `
+    <div class="alert alert-info" role="alert">
+    <p>Buen dia ${nombre.value}, nos contactaremos al correo ${email.value} para darle mas informacion</p>
+    </div>
+    `;
+});
