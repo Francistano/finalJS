9fa060a421d5166c570d1643b5d2ae88c56933fb
Francistano <111395739+Francistano@users.noreply.github.com>
2023-01-18 18:14:16 -0300

    actualizado

--- script.js
+++ script.js
@@ -29,34 +29,71 @@ const mostrarInfoForm = formulario.addEventListener("submit", function (evento){
 //Seccion de productos
 
  const sectionProductos = document.querySelector("#contenedor")
-
+ 
  fetch("./data.json")
  .then((resp) => resp.json())
  .then((data) => mostrarProds(data))
-
+ 
  function mostrarProds (prods){
-    prods.forEach(prod => {
-        const card = document.createElement('div');
-        card.innerHTML = `
-        <div class="card" style="width: 18rem">
-            <div class="card-body">
-                <h5 class="card-title">${prod.nombre}</h5>
-                <p class="card-text des">${prod.descripcion}</p>
-                <p class="card-text pre">${prod.precio}</p>
-                <button class="btn btn-primary btnComprar" id="btn${prod.id}">Comprar</button>
+     prods.forEach(prod => {
+         const card = document.createElement('div');
+         card.innerHTML = `
+         <div class="card" style="width: 18rem">
+         <div class="card-body">
+         <h5 class="card-title">${prod.nombre}</h5>
+         <p class="card-text des">${prod.descripcion}</p>
+         <p class="card-text pre">${prod.precio}</p>
+         <button class="btn btn-primary btn-comprar" id="${prod.id}">Comprar</button>
+         </div>
+         </div>
+         `
+         sectionProductos.appendChild(card);
+        });
+        const botonesComprar = document.querySelectorAll('.btn-comprar');
+        botonesComprar.forEach(btn => {
+            btn.addEventListener('click', (e)=> agregarAlCarrito(e, prods));
+            
+        })
+    }
+    
+    const carrito = document.querySelector("#carrito")
+    let prodCarrito = []
+    
+    function agregarAlCarrito(e, productos){
+        
+        const prodElegido = productos.find(el=>el.id ===parseInt( e.target.id));
+        prodCarrito = [...prodCarrito, prodElegido];
+        carritoHTML()
+    } 
+    
+    
+    function carritoHTML(){
+        actualizarCarrito()
+        prodCarrito.forEach((prod)=>{
+            let item = document.createElement("div")
+            item.innerHTML= `
+            <div class="container">
+            <h5>${prod.nombre}</h5>
+            <p>${prod.descripcion}</<p>
+            <p>${prod.precio}</<p>
+            <button class="btn btn-danger" id="${prod.id}">Eliminar</button>
             </div>
-        </div>
-      `
-        sectionProductos.appendChild(card);
-    });
-    const botonesComprar = document.querySelectorAll('.btncomprar');
-    botonesComprar.forEach(btn => {
-        btn.addEventListener('click', (e)=> agregarAlCarrito(e, prods));
+            `
+            carrito.appendChild(item)
+        })
+    }
 
-    })
- }
+    function actualizarCarrito(){
+        carrito.innerHTML=""
+    }
 
- function agregarAlCarrito(e, productos){
-    console.log(productos);
-    console.log(e.target.id);
- } 
+    
+    const eliminarDeCarrito = (prodId) => {
+        const item = carrito.find((prod) => prod.id === prodId)
+        const indice = carrito.indexOf(item)
+        carrito.splice(indice, 1)
+        actualizarCarrito()
+    }
+    console.log(prodId)
+
+    carrito.addEventListener("click", eliminarDeCarrito);
